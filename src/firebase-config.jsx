e65bfe583841a3da7dd6c0ac8bd4ae8175646a25
hlw-aryan <123iiitb@gmail.com>
2024-06-11 18:44:22 +0530

    Added CRUD operation in Buy Products Page

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -8,14 +8,15 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDyENnDIJH7vNqzCqc3Bs4CbNtokc8M5h8",
-  authDomain: "roostechdashboard.firebaseapp.com",
-  projectId: "roostechdashboard",
-  storageBucket: "roostechdashboard.appspot.com",
-  messagingSenderId: "485381350196",
-  appId: "1:485381350196:web:433b6f29b5272a4dd47b61",
-  measurementId: "G-92WWP7C40Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
